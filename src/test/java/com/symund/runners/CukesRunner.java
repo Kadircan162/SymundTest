1c8da49948878ae0bde6d0ee60baf33d43424da1
CydCoderKadircan <kadircangulde@gmail.com>
2022-02-16 18:07:21 +0300

    Symund login functionality - CukesRunner plugin renewed.

--- src/test/java/com/symund/runners/CukesRunner.java
+++ src/test/java/com/symund/runners/CukesRunner.java
@@ -8,10 +8,9 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-        plugin ={"jason:target/cucumber.json",
+        plugin = {"json:target/cucumber.json",
                 "html:target/default-html-reports",
-                "rerun:target/rerun.txt"
-        },
+                "rerun:target/rerun.txt"},
 
         features = "src/test/resources/features",
         glue = "com/symund/step_definitions",
